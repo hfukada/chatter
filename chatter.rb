030a4b315bb052f227cd14f21d4c3b925f4b817c
Graham Mackie <mackieg@umich.edu>
2013-09-21 16:40:14 +0000

    added a single sinatra call

--- chatter.rb
+++ chatter.rb
@@ -1,24 +1,40 @@
 require 'rubygems'
 require 'mongo'
+require 'sinatra'
 
 include Mongo
 
-host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
-port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
+class chatter
+    def initialize()
+        host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
+        port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
 
-puts "Connecting to #{host}:#{port}"
-db = MongoClient.new(host, port).db('local')
-coll = db.collection('users')
-# Erase all records from collection, if any
-# coll.remove
+        puts "Connecting to #{host}:#{port}"
+        db = MongoClient.new(host, port).db('mydb')
 
-# Insert 3 records
-# 3.times { |i| coll.insert({'a' => i+1}) }
+        # Erase all records from collection, if any
+        # coll.remove
 
-# puts "There are #{coll.count()} records in the test collection. Here they are:"
-coll.find().each { |doc|
-  puts doc.inspect
-}
+        # Insert 3 records
+        # 3.times { |i| coll.insert({'a' => i+1}) }
 
-# Destroy the collection
-coll.drop
+        # puts "There are #{coll.count()} records in the test collection. Here they are:"
+
+    end
+    def get_users()
+        coll = db.collection('users')
+        coll.find().each { |doc|
+            puts doc.inspect
+        }
+        coll.drop
+    end
+
+    # check if the username has been taken already
+    def check_user_exists(username)
+        db.collection('users').find({name => username}).nil?
+    end
+
+    get '/check_user:username' do
+        check_users_exists(username)
+    end
+end
