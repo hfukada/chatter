0ae5f356f5da6ba862b47a3940e55ceb6a3406f0
hfukada <hiroshi@fukada.us>
2013-09-21 12:12:15 -0400

    initial trial with mongo

--- chatter.rb
+++ chatter.rb
@@ -1,5 +1,5 @@
+require 'rubygems'
 require 'mongo'
-require 'pp'
 
 include Mongo
 
@@ -7,36 +7,18 @@ host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
 port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
 
 puts "Connecting to #{host}:#{port}"
-client  = MongoClient.new(host, port)
-db   = client.db('ruby-mongo-examples')
-coll = db.create_collection('test')
-
+db = MongoClient.new(host, port).db('local')
+coll = db.collection('users')
 # Erase all records from collection, if any
-coll.remove
+# coll.remove
 
-admin = client['admin']
+# Insert 3 records
+# 3.times { |i| coll.insert({'a' => i+1}) }
 
-# Profiling level set/get
-puts "Profiling level: #{admin.profiling_level}"
-
-# Start profiling everything
-admin.profiling_level = :all
-
-# Read records, creating a profiling event
-coll.find().to_a
-
-# Stop profiling
-admin.profiling_level = :off
-
-# Print all profiling info
-pp admin.profiling_info
-
-# Validate returns a hash if all is well and
-# raises an exception if there is a problem.
-info = db.validate_collection(coll.name)
-puts "valid = #{info['ok']}"
-puts info['result']
+# puts "There are #{coll.count()} records in the test collection. Here they are:"
+coll.find().each { |doc|
+  puts doc.inspect
+}
 
 # Destroy the collection
 coll.drop
-
